9c31b8afb533a4c997b5b5153bc8a296bf72e521
Marcin Dobosz <marcin@marcindobosz.com>
2016-08-31 18:15:30 -0700

    Add solution and project stubs

---
+++ src/xunit.analyzers/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("xUnit.net Code Analyzers")]
+[assembly: AssemblyProduct("xUnit.net Code Analyzers")]
+[assembly: AssemblyCopyright("Copyright © Marcin Dobosz")]
+[assembly: AssemblyVersion("0.0.1")]
---
+++ test/xunit.analyzers.tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("xUnit.net Code Analyzers Tests")]
+[assembly: AssemblyProduct("xUnit.net Code Analyzers Tests")]
+[assembly: AssemblyCopyright("Copyright © Marcin Dobosz")]
+[assembly: AssemblyVersion("1.0.0")]
