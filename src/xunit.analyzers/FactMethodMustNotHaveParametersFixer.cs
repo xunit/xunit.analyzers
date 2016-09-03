1cfaf4eeef9dbacdba057d01c2349c26cc38bd9a
Marcin Dobosz <marcin@marcindobosz.com>
2016-09-03 10:38:56 -0700

    Add TheoryMethodMustHaveTestDataFixer
    Create dedicated ConvertAttributeCodeAction class to handle converting attribute types

--- src/xunit.analyzers/FactMethodMustNotHaveParametersFixer.cs
+++ src/xunit.analyzers/FactMethodMustNotHaveParametersFixer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Immutable;
+﻿using System.Collections.Immutable;
 using System.Composition;
 using System.Threading;
 using System.Threading.Tasks;
@@ -8,6 +7,7 @@ using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editing;
+using Xunit.Analyzers.CodeActions;
 
 namespace Xunit.Analyzers
 {
@@ -21,21 +21,18 @@ namespace Xunit.Analyzers
 
         public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
 
-        Task<Solution> Foo(CancellationToken arg)
-        {
-            throw new NotImplementedException();
-        }
-
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
             var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
             var methodDeclaration = root.FindNode(context.Span).FirstAncestorOrSelf<MethodDeclarationSyntax>();
 
             context.RegisterCodeFix(
-                CodeAction.Create(
-                    title: convertToTheoryTitle,
-                    createChangedDocument: ct => ConvertToTheoryAsync(context.Document, methodDeclaration, ct),
-                    equivalenceKey: convertToTheoryTitle),
+                new ConvertAttributeCodeAction(
+                    convertToTheoryTitle,
+                    context.Document,
+                    methodDeclaration.AttributeLists,
+                    fromTypeName: Constants.Types.XunitFactAttribute,
+                    toTypeName: Constants.Types.XunitTheoryAttribute),
                 context.Diagnostics);
             context.RegisterCodeFix(
                 CodeAction.Create(
@@ -45,26 +42,11 @@ namespace Xunit.Analyzers
                 context.Diagnostics);
         }
 
-        async Task<Document> ConvertToTheoryAsync(Document document, MethodDeclarationSyntax methodDeclaration, CancellationToken cancellationToken)
-        {
-            var editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
-            var semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
-            foreach (var attributeList in methodDeclaration.AttributeLists)
-            {
-                foreach (var attribute in attributeList.Attributes)
-                {
-                    var s = semanticModel.GetTypeInfo(attribute).Type.ToDisplayString();
-                    if (s == Constants.Types.XunitFactAttribute)
-                        editor.ReplaceNode(attribute, editor.Generator.Attribute(Constants.Types.XunitTheoryAttribute));
-                }
-            }
-            return editor.GetChangedDocument();
-        }
-
         async Task<Document> RemoveParametersAsync(Document document, ParameterListSyntax parameterListSyntax, CancellationToken cancellationToken)
         {
             var editor = await DocumentEditor.CreateAsync(document, cancellationToken).ConfigureAwait(false);
-            editor.ReplaceNode(parameterListSyntax, parameterListSyntax.RemoveNodes(parameterListSyntax.Parameters, SyntaxRemoveOptions.KeepNoTrivia));
+            foreach (var parameter in parameterListSyntax.Parameters)
+                editor.RemoveNode(parameter);
             return editor.GetChangedDocument();
         }
     }
