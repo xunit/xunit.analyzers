28ccba26c28b302a7bba237c164c3972cd907f1c
Erik Schierboom <erik_schierboom@hotmail.com>
2017-05-08 09:53:17 +0200

    Add analyzer to convert Assert.Equal<string>, Assert.StrictEqual<string> and Assert.StrictEqual to Assert.Equal
    Closes xunit/xunit#1257

---
+++ src/xunit.analyzers/AssertEqualGenericShouldNotBeUsedForStringValue.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Xunit.Analyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class AssertEqualGenericShouldNotBeUsedForStringValue : AssertUsageAnalyzerBase
+    {
+        private static readonly HashSet<string> EqualMethods = new HashSet<string>(new[] { "Equal", "StrictEqual" });
+
+        public AssertEqualGenericShouldNotBeUsedForStringValue() :
+            base(Constants.Descriptors.X2006_AssertEqualGenericShouldNotBeUsedForStringValue, EqualMethods)
+        {
+        }
+
+        protected override void Analyze(SyntaxNodeAnalysisContext context, InvocationExpressionSyntax invocation, IMethodSymbol method)
+        {
+            if (invocation.ArgumentList.Arguments.Count != 2)
+                return;
+
+            if (!method.IsGenericMethod && method.Name == "Equal")
+                return;
+
+            if (method.IsGenericMethod &&
+               (!method.TypeArguments[0].SpecialType.Equals(SpecialType.System_String) ||
+                !method.Parameters[0].Type.SpecialType.Equals(SpecialType.System_String) || 
+                !method.Parameters[1].Type.SpecialType.Equals(SpecialType.System_String)))
+                return;
+
+            var invalidUsageDescription = method.Name == "Equal" ? "generic Assert.Equal overload" : "Assert.StrictEqual";
+            context.ReportDiagnostic(Diagnostic.Create(
+                Constants.Descriptors.X2006_AssertEqualGenericShouldNotBeUsedForStringValue,
+                invocation.GetLocation(),
+                invalidUsageDescription));
+        }
+    }
+}
