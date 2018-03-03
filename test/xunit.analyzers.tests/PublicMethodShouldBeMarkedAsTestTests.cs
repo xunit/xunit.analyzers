d2ccb247175b91a301323664973fbe29db9d4ed5
Adam Ralph <adam@adamralph.com>
2018-03-03 12:08:26 +0100

    add test for attribute derived from 1013 ignore attribute

--- test/xunit.analyzers.tests/PublicMethodShouldBeMarkedAsTestTests.cs
+++ test/xunit.analyzers.tests/PublicMethodShouldBeMarkedAsTestTests.cs
@@ -141,6 +141,28 @@ public class TestClass { [Xunit.Fact] public void TestMethod() { } [CustomTestTy
             Assert.Empty(diagnostics);
         }
 
+        [Fact]
+        public async void FindsWarningForPublicMethodMarkedWithAttributeWhichInheritsFromAttributeMarkedWithIgnoreXunitAnalyzersRule1013()
+        {
+            var diagnostics = await CodeAnalyzerHelper.GetDiagnosticsAsync(analyzer,
+@"public class IgnoreXunitAnalyzersRule1013Attribute : System.Attribute { }
+
+[IgnoreXunitAnalyzersRule1013]
+public class BaseCustomTestTypeAttribute : System.Attribute { }
+
+public class DerivedCustomTestTypeAttribute : BaseCustomTestTypeAttribute { }
+
+public class TestClass { [Xunit.Fact] public void TestMethod() { } [DerivedCustomTestType] public void CustomTestMethod() {} }");
+
+            Assert.Collection(diagnostics,
+                d =>
+                {
+                    Assert.Equal("Public method 'CustomTestMethod' on test class 'TestClass' should be marked as a Fact.", d.GetMessage());
+                    Assert.Equal("xUnit1013", d.Id);
+                    Assert.Equal(DiagnosticSeverity.Warning, d.Severity);
+                });
+        }
+
         [Theory]
         [InlineData("Xunit.Fact")]
         [InlineData("Xunit.Theory")]
