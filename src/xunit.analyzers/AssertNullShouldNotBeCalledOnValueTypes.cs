244f406d70af0d72756f4db8c372dac65f60c573
Marcin Wachulski <marcin.wachulski@gmail.com>
2018-03-08 14:11:25 +0100

    Refactor AssertNullShouldNotBeCalledOnValueTypes

--- src/xunit.analyzers/AssertNullShouldNotBeCalledOnValueTypes.cs
+++ src/xunit.analyzers/AssertNullShouldNotBeCalledOnValueTypes.cs
@@ -9,8 +9,8 @@ namespace Xunit.Analyzers
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class AssertNullShouldNotBeCalledOnValueTypes : AssertUsageAnalyzerBase
     {
-        internal const string NullMethod = "Null";
-        internal const string NotNullMethod = "NotNull";
+        private const string NullMethod = "Null";
+        private const string NotNullMethod = "NotNull";
 
         public AssertNullShouldNotBeCalledOnValueTypes() : base(
             Descriptors.X2002_AssertNullShouldNotBeCalledOnValueTypes,
@@ -23,18 +23,32 @@ namespace Xunit.Analyzers
             if (invocation.ArgumentList.Arguments.Count != 1)
                 return;
 
-            var argumentType = context.SemanticModel.GetTypeInfo(invocation.ArgumentList.Arguments.Single().Expression, context.CancellationToken).Type;
-            if (argumentType == null ||
-               argumentType.IsReferenceType ||
-               (argumentType.OriginalDefinition.SpecialType == SpecialType.System_Nullable_T) ||
-                (!argumentType.IsReferenceType && !argumentType.IsValueType)) // unconstrained generic type case
+            var argumentExpression = invocation.ArgumentList.Arguments.Single().Expression;
+            var argumentType = context.SemanticModel.GetTypeInfo(argumentExpression, context.CancellationToken).Type;
+            
+            if (argumentType == null || IsArgumentTypeRecognizedAsReferenceType(argumentType))
                 return;
 
             context.ReportDiagnostic(Diagnostic.Create(
                 Descriptors.X2002_AssertNullShouldNotBeCalledOnValueTypes,
                 invocation.GetLocation(),
-                SymbolDisplay.ToDisplayString(method, SymbolDisplayFormat.CSharpShortErrorMessageFormat.WithParameterOptions(SymbolDisplayParameterOptions.None)),
-                SymbolDisplay.ToDisplayString(argumentType, SymbolDisplayFormat.CSharpShortErrorMessageFormat.WithParameterOptions(SymbolDisplayParameterOptions.None))));
+                GetDisplayString(method),
+                GetDisplayString(argumentType)));
+        }
+
+        private static bool IsArgumentTypeRecognizedAsReferenceType(ITypeSymbol argumentType)
+        {
+            var isNullableOfT = argumentType.OriginalDefinition.SpecialType == SpecialType.System_Nullable_T;
+            var isUnconstrainedGenericType = !argumentType.IsReferenceType && !argumentType.IsValueType;
+            
+            return argumentType.IsReferenceType || isNullableOfT || isUnconstrainedGenericType;
+        }
+
+        private static string GetDisplayString(ISymbol method)
+        {
+            var displayFormat = SymbolDisplayFormat.CSharpShortErrorMessageFormat.WithParameterOptions(
+                SymbolDisplayParameterOptions.None);
+            return SymbolDisplay.ToDisplayString(method, displayFormat);
         }
     }
 }
