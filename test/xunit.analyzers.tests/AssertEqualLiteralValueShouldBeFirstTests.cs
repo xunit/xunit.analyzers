ad41776f96f03ad04c7dec5dcec500026795e263
Thijs Brobbel <thijsbrobbel@gmail.com>
2018-05-16 14:19:55 +0200

    Account for named arguments in xUnit2000:
    literal value should be first argument. This handles a case where named
    arguments are used, which are swapped.

--- test/xunit.analyzers.tests/AssertEqualLiteralValueShouldBeFirstTests.cs
+++ test/xunit.analyzers.tests/AssertEqualLiteralValueShouldBeFirstTests.cs
@@ -60,5 +60,41 @@ namespace Xunit.Analyzers
                 Assert.Equal(DiagnosticSeverity.Warning, d.Severity);
             });
         }
+
+        [Theory]
+        [MemberData(nameof(TypesAndValues))]
+        public async void DoesNotFindWarningForExpectedConstantOrLiteralValueAsNamedExpectedArgument(string type, string value)
+        {
+            var diagnostics = await CodeAnalyzerHelper.GetDiagnosticsAsync(analyzer,
+@"class TestClass { void TestMethod() { 
+    var v = default(" + type + @");
+    Xunit.Assert.Equal(actual: v, expected: " + value + @");
+} }");
+
+            Assert.Empty(diagnostics);
+        }
+
+        [Theory]
+        [MemberData(nameof(TypesAndValues))]
+        public async void FindsWarningForExpectedConstantOrLiteralValueAsNamedExpectedArgument(string type, string value)
+        {
+            var diagnostics = await CodeAnalyzerHelper.GetDiagnosticsAsync(analyzer,
+@"class TestClass { void TestMethod() { 
+    var v = default(" + type + @");
+    Xunit.Assert.Equal(actual: " + value + @",expected: v);
+} }");
+
+            Assert.Collection(diagnostics, d =>
+            {
+                Assert.Equal($"The literal or constant value {value} should be the first argument in the call to 'Assert.Equal(expected, actual)' in method 'TestMethod' on type 'TestClass'.", d.GetMessage());
+                Assert.Equal("xUnit2000", d.Id);
+                Assert.Equal(DiagnosticSeverity.Warning, d.Severity);
+            });
+        }
+
+        // TODO: handle case where expected or actual is not present on argument list
+        // where expected or actual is multiple times on the list
+        // fix documentation
+
     }
 }
