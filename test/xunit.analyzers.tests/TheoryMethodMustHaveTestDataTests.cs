f9beee02c903b2ff59cf784466f8907cc5c0c7b4
Sam Harwell <sam@tunnelvisionlabs.com>
2019-09-16 11:23:50 -0700

    Update TheoryMethodMustHaveTestData to support Visual Basic

--- test/xunit.analyzers.tests/TheoryMethodMustHaveTestDataTests.cs
+++ test/xunit.analyzers.tests/TheoryMethodMustHaveTestDataTests.cs
@@ -1,4 +1,5 @@
 ﻿using VerifyCS = Xunit.Analyzers.CSharpVerifier<Xunit.Analyzers.TheoryMethodMustHaveTestData>;
+using VerifyVB = Xunit.Analyzers.VisualBasicVerifier<Xunit.Analyzers.TheoryMethodMustHaveTestData>;
 
 namespace Xunit.Analyzers
 {
@@ -10,6 +11,19 @@ namespace Xunit.Analyzers
             var source = "public class TestClass { [Xunit.Fact] public void TestMethod() { } }";
 
             await VerifyCS.VerifyAnalyzerAsync(source);
+        }
+
+        [Fact]
+        public async void DoesNotFindErrorForFactMethod_VisualBasic()
+        {
+            var source = @"
+Public Class TestClass
+    <Xunit.Fact>
+    Public Sub TestMethod()
+    End Sub
+End Class";
+
+            await VerifyVB.VerifyAnalyzerAsync(source);
         }
 
         [Theory]
@@ -24,6 +38,22 @@ namespace Xunit.Analyzers
             await VerifyCS.VerifyAnalyzerAsync(source);
         }
 
+        [Theory]
+        [InlineData("InlineData")]
+        [InlineData("MemberData(\"\")")]
+        [InlineData("ClassData(GetType(String))")]
+        public async void DoesNotFindErrorForTheoryMethodWithDataAttributes_VisualBasic(string dataAttribute)
+        {
+            var source = $@"
+Public Class TestClass
+    <Xunit.Theory, Xunit.{dataAttribute}>
+    Public Sub TestMethod()
+    End Sub
+End Class";
+
+            await VerifyVB.VerifyAnalyzerAsync(source);
+        }
+
         [Fact]
         public async void FindsErrorForTheoryMethodMissingData_CSharp()
         {
@@ -31,6 +61,20 @@ namespace Xunit.Analyzers
 
             var expected = VerifyCS.Diagnostic().WithSpan(1, 46, 1, 56);
             await VerifyCS.VerifyAnalyzerAsync(source, expected);
+        }
+
+        [Fact]
+        public async void FindsErrorForTheoryMethodMissingData_VisualBasic()
+        {
+            var source = @"
+Class TestClass
+    <Xunit.Theory>
+    Public Sub TestMethod()
+    End Sub
+End Class";
+
+            var expected = VerifyVB.Diagnostic().WithSpan(4, 16, 4, 26);
+            await VerifyVB.VerifyAnalyzerAsync(source, expected);
         }
     }
 }
