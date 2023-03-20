ff43df6a3ee2e48cbadc259ca9930d600cab4c24
Björn Hellander <bjorn.hellander@gmail.com>
2023-03-20 21:23:44 +0100

    Update RemoveMethodParameterFix to not crash when parameter declaration is missing (#156)

--- src/xunit.analyzers.tests/Fixes/FixProviders/RemoveMethodParameterFixTests.cs
+++ src/xunit.analyzers.tests/Fixes/FixProviders/RemoveMethodParameterFixTests.cs
@@ -80,6 +80,53 @@ public class TestClass {
 		await Verify_X1026.VerifyCodeFixAsyncV2(before, after);
 	}
 
+	[Fact]
+	public async void X1026_DoesNotCrashWhenParameterDeclarationIsMissing()
+	{
+		var before = @"
+using Xunit;
+
+public class TestClass {
+	[Theory]
+	[InlineData(1, 1)]
+	public void Test1(int x, )
+	{
+		var x1 = x;
+	}
+}";
+
+		var after = @"
+using Xunit;
+
+public class TestClass {
+	[Theory]
+	[InlineData(1, 1)]
+	public void Test1(int x, )
+	{
+		var x1 = x;
+	}
+}";
+
+		var expected = new[]
+		{
+			Verify_X1026
+				.Diagnostic()
+				.WithSpan(7, 27, 7, 27)
+				.WithSeverity(DiagnosticSeverity.Warning)
+				.WithArguments("Test1", "TestClass", ""),
+			Verify_X1026
+				.CompilerError("CS1001")
+				.WithSpan(7, 27, 7, 28)
+				.WithMessage("Identifier expected"),
+			Verify_X1026
+				.CompilerError("CS1031")
+				.WithSpan(7, 27, 7, 28)
+				.WithMessage("Type expected"),
+		};
+
+		await Verify_X1026.VerifyCodeFixAsyncV2(before, after, diagnostics: expected);
+	}
+
 	internal class Analyzer_X1022 : TheoryMethodCannotHaveParamsArray
 	{
 		protected override XunitContext CreateXunitContext(Compilation compilation) =>
