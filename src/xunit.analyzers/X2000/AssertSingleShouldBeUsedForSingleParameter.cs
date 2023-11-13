633ac11c18851eab7ef35e4ac0e18ff9a4b00668
etherfield <148793545+etherfield@users.noreply.github.com>
2023-11-13 17:18:33 +0300

    Single-item collection analyzer

---
+++ src/xunit.analyzers/X2000/AssertSingleShouldBeUsedForSingleParameter.cs
@@ -0,0 +1,47 @@
+using Microsoft.CodeAnalysis.Diagnostics;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace Xunit.Analyzers;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public class AssertSingleShouldBeUsedForSingleParameter : AssertUsageAnalyzerBase
+{
+	static readonly string[] targetMethods =
+	{
+		Constants.Asserts.Collection
+	};
+
+	public AssertSingleShouldBeUsedForSingleParameter() :
+		base(Descriptors.X2023_AssertSingleShouldBeUsedForSingleParameter, targetMethods)
+	{ }
+
+	protected override void AnalyzeInvocation(
+		OperationAnalysisContext context,
+		XunitContext xunitContext,
+		IInvocationOperation invocationOperation,
+		IMethodSymbol method)
+	{
+		if (invocationOperation.Arguments.Length != 2)
+			return;
+
+		var secondParameter = invocationOperation.Arguments[1];
+		if (secondParameter.ArgumentKind != ArgumentKind.ParamArray)
+			return;
+
+		if (secondParameter.Value is not IArrayCreationOperation operation)
+			return;
+
+		if (operation.DimensionSizes.Length != 1 || (int)(operation.DimensionSizes[0].ConstantValue.Value ?? 0) != 1)
+			return;
+
+		context.ReportDiagnostic(
+			Diagnostic.Create(
+				Descriptors.X2023_AssertSingleShouldBeUsedForSingleParameter,
+				invocationOperation.Syntax.GetLocation(),
+				method.Name
+			)
+		);
+
+	}
+}
