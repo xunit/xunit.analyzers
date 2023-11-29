aa8b0c5a049266acd5055c9191003cb658017c69
etherfield <148793545+etherfield@users.noreply.github.com>
2023-11-29 16:28:12 +0300

    Tests

--- src/xunit.analyzers.tests/Analyzers/X1000/CollectionDefinitionMustBeInTheSameAssemblyTests.cs
+++ src/xunit.analyzers.tests/Analyzers/X1000/CollectionDefinitionMustBeInTheSameAssemblyTests.cs
@@ -4,38 +4,69 @@ using Verify = CSharpVerifier<Xunit.Analyzers.CollectionDefinitionMustBeInTheSam
 
 public class CollectionDefinitionMustBeInTheSameAssemblyTests
 {
-	public static TheoryData<string, string> NoDiagnosticsCases = new()
+	public static TheoryData<string, string, string> BasicNoDiagnosticsCases = new()
 	{
 		{
 			"[Collection(\"Test collection definition\")]",
-			"[CollectionDefinition(\"Test collection definition\")]"
+			"[CollectionDefinition(\"Test collection definition\")]",
+			"TestFixture fixture"
 		},
 		{
 			string.Empty,
-			"[CollectionDefinition(\"Test collection definition\")]"
+			"[CollectionDefinition(\"Test collection definition\")]",
+			"TestFixture fixture"
 		},
 		{
+			string.Empty,
+			string.Empty,
+			"TestFixture fixture"
+		},
+		{
+			"[Collection(\"No fixture required collection definition\")]",
+			"[CollectionDefinition(\"Test collection definition\")]",
+			string.Empty
+		},
+		{
+			string.Empty,
 			string.Empty,
 			string.Empty
-		}
+		},
+		{
+			string.Empty,
+			"[CollectionDefinition(\"Test collection definition\")]",
+			string.Empty
+		},
+		{
+			"[Collection(\"No fixture required collection definition\")]",
+			string.Empty,
+			string.Empty
+		},
 	};
 
-	static readonly string Template = @"
+	static readonly string BasicCasesTemplate = @"
 using Xunit;
 
 {0}
-public class TestClass {{ }}
+public class TestClass
+{{
+    public TestClass({2}) {{ }}
+}}
+
+public class TestFixture {{ }}
 
 namespace TestNamespace {{
     {1}
-    public class TestDefinition {{ }}
+    public class TestDefinition : ICollectionFixture<TestFixture> {{ }}
 }}";
 
 	[Theory]
-	[MemberData(nameof(NoDiagnosticsCases))]
-	public async void CollectionDefinitionIsPresentInTheAssembly_NoDiagnostics(string classAttribute, string definitionAttribute)
+	[MemberData(nameof(BasicNoDiagnosticsCases))]
+	public async void CollectionDefinitionIsPresentInTheAssembly_NoDiagnostics(
+		string classAttribute,
+		string definitionAttribute,
+		string classConstructorParams)
 	{
-		var source = string.Format(Template, classAttribute, definitionAttribute);
+		var source = string.Format(BasicCasesTemplate, classAttribute, definitionAttribute, classConstructorParams);
 
 		await Verify.VerifyAnalyzer(source);
 	}
@@ -43,13 +74,13 @@ namespace TestNamespace {{
 	[Fact]
 	public async void CollectionDefinitionIsMissingInTheAssembly_ReturnsError()
 	{
-		var source = string.Format(Template, "[Collection(\"Test collection definition\")]", string.Empty);
+		var source = string.Format(BasicCasesTemplate, "[Collection(\"Test collection definition\")]", string.Empty, "TestFixture fixture");
 
 		var expected =
 			Verify
 				.Diagnostic()
 				.WithSpan(5, 14, 5, 23)
-				.WithSeverity(DiagnosticSeverity.Hidden)
+				.WithSeverity(DiagnosticSeverity.Error)
 				.WithArguments("Test collection definition", "TestProject");
 
 		await Verify.VerifyAnalyzer(source, expected);
