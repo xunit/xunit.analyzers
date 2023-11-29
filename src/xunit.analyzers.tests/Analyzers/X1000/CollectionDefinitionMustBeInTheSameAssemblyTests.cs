fce1ee5b044c46c152bcd752a07b2b1f37d219f7
etherfield <148793545+etherfield@users.noreply.github.com>
2023-11-29 17:09:03 +0300

    Tests for collection definition inheritance

--- src/xunit.analyzers.tests/Analyzers/X1000/CollectionDefinitionMustBeInTheSameAssemblyTests.cs
+++ src/xunit.analyzers.tests/Analyzers/X1000/CollectionDefinitionMustBeInTheSameAssemblyTests.cs
@@ -43,6 +43,54 @@ public class CollectionDefinitionMustBeInTheSameAssemblyTests
 		},
 	};
 
+	public static TheoryData<string, string, string> InheritedDefinitionNoDiagnosticsCases = new()
+	{
+		{
+			string.Empty,
+			", ICollectionFixture<TestFixture>",
+			": ICollectionFixture<AnotherTestFixture>"
+		},
+		{
+			"TestFixture fixture",
+			", ICollectionFixture<TestFixture>",
+			": ICollectionFixture<AnotherTestFixture>"
+		},
+		{
+			"AnotherTestFixture anotherFixture",
+			", ICollectionFixture<TestFixture>",
+			": ICollectionFixture<AnotherTestFixture>"
+		},
+		{
+			"TestFixture fixture, AnotherTestFixture anotherFixture",
+			", ICollectionFixture<TestFixture>",
+			": ICollectionFixture<AnotherTestFixture>"
+		},
+		{
+			"TestFixture fixture, AnotherTestFixture anotherFixture",
+			", ICollectionFixture<TestFixture>, ICollectionFixture<AnotherTestFixture>",
+			string.Empty
+		},
+		{
+			"TestFixture fixture, AnotherTestFixture anotherFixture",
+			string.Empty,
+			": ICollectionFixture<TestFixture>, ICollectionFixture<AnotherTestFixture>"
+		},
+	};
+
+	public static TheoryData<string, string, string> InheritedDefinitionErrorCases = new()
+	{
+		{
+			"TestFixture fixture, AnotherTestFixture anotherFixture",
+			string.Empty,
+			": ICollectionFixture<AnotherTestFixture>"
+		},
+		{
+			"TestFixture fixture, AnotherTestFixture anotherFixture",
+			", ICollectionFixture<TestFixture>",
+			string.Empty
+		},
+	};
+
 	static readonly string BasicCasesTemplate = @"
 using Xunit;
 
@@ -59,11 +107,30 @@ namespace TestNamespace {{
     public class TestDefinition : ICollectionFixture<TestFixture> {{ }}
 }}";
 
+	static readonly string InheritedDefinitionTemplate = @"
+using Xunit;
+
+[Collection(""Test collection definition"")]
+public class TestClass
+{{
+    public TestClass({0}) {{ }}
+}}
+
+public class TestFixture {{ }}
+public class AnotherTestFixture {{ }}
+
+namespace TestNamespace {{
+	public class BaseTestDefinition {2} {{ }}
+
+    [CollectionDefinition(""Test collection definition"")]
+    public class TestDefinition : BaseTestDefinition {1} {{ }}
+}}";
+
 	[Theory]
 	[MemberData(nameof(BasicNoDiagnosticsCases))]
-	public async void CollectionDefinitionIsPresentInTheAssembly_NoDiagnostics(
+	public async void BasicCollectionDefinitionIsPresentInTheAssembly_NoDiagnostics(
 		string classAttribute,
-		string definitionAttribute,
+		string definitionAttribute, 
 		string classConstructorParams)
 	{
 		var source = string.Format(BasicCasesTemplate, classAttribute, definitionAttribute, classConstructorParams);
@@ -71,10 +138,41 @@ namespace TestNamespace {{
 		await Verify.VerifyAnalyzer(source);
 	}
 
+	[Theory]
+	[MemberData(nameof(InheritedDefinitionNoDiagnosticsCases))]
+	public async void InheritedCollectionDefinitionIsPresentInTheAssembly_NoDiagnostics(
+		string classConstructorParams,
+		string definitionInherited,
+		string baseDefinitionInherited)
+	{
+		var source = string.Format(InheritedDefinitionTemplate, classConstructorParams, definitionInherited, baseDefinitionInherited);
+
+		await Verify.VerifyAnalyzer(source);
+	}
+
 	[Fact]
-	public async void CollectionDefinitionIsMissingInTheAssembly_ReturnsError()
+	public async void BasicCollectionDefinitionIsMissingInTheAssembly_ReturnsError()
 	{
 		var source = string.Format(BasicCasesTemplate, "[Collection(\"Test collection definition\")]", string.Empty, "TestFixture fixture");
+
+		var expected =
+			Verify
+				.Diagnostic()
+				.WithSpan(5, 14, 5, 23)
+				.WithSeverity(DiagnosticSeverity.Error)
+				.WithArguments("Test collection definition", "TestProject");
+
+		await Verify.VerifyAnalyzer(source, expected);
+	}
+
+	[Theory]
+	[MemberData(nameof(InheritedDefinitionErrorCases))]
+	public async void InheritedCollectionDefinitionIsMissingInTheAssembly_ReturnsError(
+		string classConstructorParams,
+		string definitionInherited,
+		string baseDefinitionInherited)
+	{
+		var source = string.Format(InheritedDefinitionTemplate, classConstructorParams, definitionInherited, baseDefinitionInherited);
 
 		var expected =
 			Verify
