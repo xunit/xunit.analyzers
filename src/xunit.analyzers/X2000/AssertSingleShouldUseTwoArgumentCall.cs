21560dd9d30c497453526a122b3c8dff0bb6551b
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-29 20:30:06 +0200

    Implement analyzer

--- src/xunit.analyzers/X2000/AssertSingleShouldUseTwoArgumentCall.cs
+++ src/xunit.analyzers/X2000/AssertSingleShouldUseTwoArgumentCall.cs
@@ -1,4 +1,5 @@
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Operations;
 
@@ -7,6 +8,8 @@ namespace Xunit.Analyzers;
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
 public class AssertSingleShouldUseTwoArgumentCall : AssertUsageAnalyzerBase
 {
+	const string linqWhereMethod = "System.Linq.Enumerable.Where<TSource>(System.Collections.Generic.IEnumerable<TSource>, System.Func<TSource, bool>)";
+
 	static readonly string[] targetMethods =
 	[
 		Constants.Asserts.Single,
@@ -22,6 +25,38 @@ public class AssertSingleShouldUseTwoArgumentCall : AssertUsageAnalyzerBase
 		IInvocationOperation invocationOperation,
 		IMethodSymbol method)
 	{
-		return;
+		Guard.ArgumentNotNull(xunitContext);
+		Guard.ArgumentNotNull(invocationOperation);
+		Guard.ArgumentNotNull(method);
+
+		var arguments = invocationOperation.Arguments;
+		if (arguments.Length != 1)
+			return;
+
+		var argument = arguments[0];
+		var value = argument.Value;
+		if (value is IConversionOperation conversion)
+			value = conversion.Operand;
+
+		if (value is not IInvocationOperation innerInvocation)
+			return;
+
+		var originalMethod = SymbolDisplay.ToDisplayString(innerInvocation.TargetMethod.OriginalDefinition);
+		if (originalMethod != linqWhereMethod)
+			return;
+
+		context.ReportDiagnostic(
+			Diagnostic.Create(
+				Descriptors.X2031_AssertSingleShouldUseTwoArgumentCall,
+				invocationOperation.Syntax.GetLocation(),
+				SymbolDisplay.ToDisplayString(
+					method,
+					SymbolDisplayFormat
+						.CSharpShortErrorMessageFormat
+						.WithParameterOptions(SymbolDisplayParameterOptions.None)
+						.WithGenericsOptions(SymbolDisplayGenericsOptions.None)
+				)
+			)
+		);
 	}
 }
