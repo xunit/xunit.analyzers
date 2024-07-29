9c996e62a226995db155b96c49d72b8c10fa186c
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-29 20:22:52 +0200

    Add tests for analyzer

--- src/xunit.analyzers.tests/Analyzers/X2000/AssertSingleShouldUseTwoArgumentCallTests.cs
+++ src/xunit.analyzers.tests/Analyzers/X2000/AssertSingleShouldUseTwoArgumentCallTests.cs
@@ -4,4 +4,112 @@ using Verify = CSharpVerifier<Xunit.Analyzers.AssertSingleShouldUseTwoArgumentCa
 
 public class AssertSingleShouldUseTwoArgumentCallTests
 {
+	public static TheoryData<string, string> GetEnumerables(
+		string typeName,
+		string comparison) =>
+			new()
+			{
+				{ $"new System.Collections.Generic.List<{typeName}>()", comparison },
+				{ $"new System.Collections.Generic.HashSet<{typeName}>()", comparison },
+				{ $"new System.Collections.ObjectModel.Collection<{typeName}>()", comparison },
+				{ $"new {typeName}[0]", comparison },
+				{ $"System.Linq.Enumerable.Empty<{typeName}>()", comparison },
+			};
+
+	[Theory]
+	[MemberData(nameof(GetEnumerables), "int", "")]
+	[MemberData(nameof(GetEnumerables), "string", "")]
+	public async Task Containers_WithoutWhereClause_DoesNotTrigger(
+		string collection,
+		string _)
+	{
+		var source = string.Format(/* lang=c#-test */ """
+			class TestClass {{
+			    void TestMethod() {{
+					Xunit.Assert.Single({0});
+			    }}
+			}}
+			""", collection);
+
+		await Verify.VerifyAnalyzer(source);
+	}
+
+	[Theory]
+	[MemberData(nameof(GetEnumerables), "int", "f > 0")]
+	[MemberData(nameof(GetEnumerables), "string", "f.Length > 0")]
+	public async Task Containers_WithWhereClauseWithIndex_DoesNotTrigger(
+		string collection,
+		string comparison)
+	{
+		var source = string.Format(/* lang=c#-test */ """
+			using System.Linq;
+
+			class TestClass {{
+			    void TestMethod() {{
+					Xunit.Assert.Single({0}.Where((f, i) => {1} && i > 0));
+			    }}
+			}}
+			""", collection, comparison);
+
+		await Verify.VerifyAnalyzer(source);
+	}
+
+	[Theory]
+	[MemberData(nameof(GetEnumerables), "int", "f > 0")]
+	[MemberData(nameof(GetEnumerables), "string", "f.Length > 0")]
+	public async Task EnumurableEmptyCheck_WithChainedLinq_DoesNotTrigger(
+		string collection,
+		string comparison)
+	{
+		var source = string.Format(/* lang=c#-test */ """
+			using System.Linq;
+
+			class TestClass {{
+			    void TestMethod() {{
+					Xunit.Assert.Single({0}.Where(f => {1}).Select(f => f));
+			    }}
+			}}
+			""", collection, comparison);
+
+		await Verify.VerifyAnalyzer(source);
+	}
+
+	[Theory]
+	[MemberData(nameof(GetEnumerables), "int", "f > 0")]
+	[MemberData(nameof(GetEnumerables), "string", "f.Length > 0")]
+	public async Task Containers_WithWhereClause_Triggers(
+		string collection,
+		string comparison)
+	{
+		var source = string.Format(/* lang=c#-test */ """
+			using System.Linq;
+
+			class TestClass {{
+			    void TestMethod() {{
+					[|Xunit.Assert.Single({0}.Where(f => {1}))|];
+			    }}
+			}}
+			""", collection, comparison);
+
+		await Verify.VerifyAnalyzer(source);
+	}
+
+	[Theory]
+	[InlineData("")]
+	[InlineData("123")]
+	[InlineData(@"abc\n\t\\\""")]
+	public async Task Strings_WithWhereClause_Triggers(string sampleString)
+	{
+		var source = string.Format(/* lang=c#-test */ """
+			using System.Linq;
+
+			class TestClass {{
+			    void TestMethod() {{
+					[|Xunit.Assert.Single("{0}".Where(f => f > 0))|];
+			    }}
+			}}
+			""", sampleString);
+
+		await Verify.VerifyAnalyzer(source);
+	}
 }
