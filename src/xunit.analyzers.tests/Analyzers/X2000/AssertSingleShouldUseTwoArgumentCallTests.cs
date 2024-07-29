b4904e8f7ae453754a675265f3eae2a17391c06c
RieBi <42877671+RieBi@users.noreply.github.com>
2024-07-29 20:11:07 +0200

    Add class for analyzer tests

---
+++ src/xunit.analyzers.tests/Analyzers/X2000/AssertSingleShouldUseTwoArgumentCallTests.cs
@@ -0,0 +1,7 @@
+using System.Threading.Tasks;
+using Xunit;
+using Verify = CSharpVerifier<Xunit.Analyzers.AssertSingleShouldUseTwoArgumentCall>;
+
+public class AssertSingleShouldUseTwoArgumentCallTests
+{
+}
