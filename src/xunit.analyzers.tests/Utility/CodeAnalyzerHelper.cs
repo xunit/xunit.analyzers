d27b1b0553139b5a54e9c83bd42c91a0808085a5
Brad Wilson <dotnetguy@gmail.com>
2025-01-10 19:34:39 -0800

    Latest dependencies

--- src/xunit.analyzers.tests/Utility/CodeAnalyzerHelper.cs
+++ src/xunit.analyzers.tests/Utility/CodeAnalyzerHelper.cs
@@ -61,10 +61,10 @@ static class CodeAnalyzerHelper
 				new PackageIdentity("Microsoft.Extensions.Primitives", "8.0.0"),
 				new PackageIdentity("System.Threading.Tasks.Extensions", "4.5.4"),
 				new PackageIdentity("System.Text.Json", "8.0.0"),
-				new PackageIdentity("xunit.v3.assert", "1.0.1-pre.14"),
-				new PackageIdentity("xunit.v3.common", "1.0.1-pre.14"),
-				new PackageIdentity("xunit.v3.extensibility.core", "1.0.1-pre.14"),
-				new PackageIdentity("xunit.v3.runner.common", "1.0.1-pre.14")
+				new PackageIdentity("xunit.v3.assert", "1.0.2-pre.3"),
+				new PackageIdentity("xunit.v3.common", "1.0.2-pre.3"),
+				new PackageIdentity("xunit.v3.extensibility.core", "1.0.2-pre.3"),
+				new PackageIdentity("xunit.v3.runner.common", "1.0.2-pre.3")
 			)
 		);
 
@@ -74,8 +74,8 @@ static class CodeAnalyzerHelper
 				new PackageIdentity("Microsoft.Extensions.Primitives", "8.0.0"),
 				new PackageIdentity("System.Threading.Tasks.Extensions", "4.5.4"),
 				new PackageIdentity("System.Text.Json", "8.0.0"),
-				new PackageIdentity("xunit.v3.common", "1.0.1-pre.14"),
-				new PackageIdentity("xunit.v3.runner.utility", "1.0.1-pre.14")
+				new PackageIdentity("xunit.v3.common", "1.0.2-pre.3"),
+				new PackageIdentity("xunit.v3.runner.utility", "1.0.2-pre.3")
 			)
 		);
 	}
