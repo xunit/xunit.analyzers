4aae1b57b68e25a54573cea9ca69a6973b505871
joaonunomota <jnuno.carrilho@gmail.com>
2025-04-23 22:32:23 +0100

    Update xUnit1004 to not trigger with SkipWhen or SkipUnless

--- src/xunit.analyzers.tests/Analyzers/X1000/TestMethodShouldNotBeSkippedTests.cs
+++ src/xunit.analyzers.tests/Analyzers/X1000/TestMethodShouldNotBeSkippedTests.cs
@@ -1,4 +1,5 @@
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.CSharp;
 using Xunit;
 using Verify = CSharpVerifier<Xunit.Analyzers.TestMethodShouldNotBeSkipped>;
 
@@ -32,5 +33,37 @@ public class TestMethodShouldNotBeSkippedTests
 			""", attribute);
 
 		await Verify.VerifyAnalyzer(source);
+	}
+
+	[Theory]
+	[InlineData("Fact")]
+	[InlineData("Theory")]
+	public async Task SkippedTestWhenConditionIsTrue_V3_DoesNotTrigger(string attribute)
+	{
+		var source = string.Format(/* lang=c#-test */ """
+			public class TestClass {{
+				public static bool Condition {{ get; set; }}
+				[Xunit.{0}(Skip="Lazy", SkipWhen=nameof(Condition))]
+				public void TestMethod() {{ }}
+			}}
+			""", attribute);
+
+		await Verify.VerifyAnalyzerV3(LanguageVersion.CSharp7, source);
+	}
+
+	[Theory]
+	[InlineData("Fact")]
+	[InlineData("Theory")]
+	public async Task SkippedTestUnlessConditionIsTrue_V3_DoesNotTrigger(string attribute)
+	{
+		var source = string.Format(/* lang=c#-test */ """
+			public class TestClass {{
+				public static bool Condition {{ get; set; }}
+				[Xunit.{0}(Skip="Lazy", SkipUnless=nameof(Condition))]
+				public void TestMethod() {{ }}
+			}}
+			""", attribute);
+
+		await Verify.VerifyAnalyzerV3(LanguageVersion.CSharp7, source);
 	}
 }
