c3b3685bbbf8bd16f334aa3e1ce7edfa97cc675d
James Hughes <james@pyrosoftsolutions.co.uk>
2026-02-12 01:17:23 +0000

    Enabled Fix All provider for AssertEqualShouldNotBeUsedForBoolLiteralCheck and replaced single-fix test with multi-fix test

--- src/xunit.analyzers.fixes/X2000/AssertEqualShouldNotBeUsedForBoolLiteralCheckFixer.cs
+++ src/xunit.analyzers.fixes/X2000/AssertEqualShouldNotBeUsedForBoolLiteralCheckFixer.cs
@@ -7,6 +7,7 @@ using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editing;
+using static Microsoft.CodeAnalysis.CodeFixes.WellKnownFixAllProviders;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Xunit.Analyzers.Fixes;
@@ -19,6 +20,8 @@ public class AssertEqualShouldNotBeUsedForBoolLiteralCheckFixer : XunitCodeFixPr
 	public AssertEqualShouldNotBeUsedForBoolLiteralCheckFixer() :
 		base(Descriptors.X2004_AssertEqualShouldNotUsedForBoolLiteralCheck.Id)
 	{ }
+
+	public override FixAllProvider? GetFixAllProvider() => BatchFixer;
 
 	public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
 	{
--- src/xunit.analyzers.tests/Fixes/X2000/AssertEqualShouldNotBeUsedForBoolLiteralCheckFixerTests.cs
+++ src/xunit.analyzers.tests/Fixes/X2000/AssertEqualShouldNotBeUsedForBoolLiteralCheckFixerTests.cs
@@ -5,51 +5,36 @@ using Verify = CSharpVerifier<Xunit.Analyzers.AssertEqualShouldNotBeUsedForBoolL
 
 public class AssertEqualShouldNotBeUsedForBoolLiteralCheckFixerTests
 {
-	const string template = /* lang=c#-test */ """
-		using Xunit;
-
-		public class TestClass {{
-			[Fact]
-			public void TestMethod() {{
-				var actual = true;
-
-				{0};
-			}}
-		}}
-		""";
-
-	[Theory]
-	[InlineData(
-		/* lang=c#-test */ "[|Assert.Equal(false, actual)|]",
-		/* lang=c#-test */ "Assert.False(actual)")]
-	[InlineData(
-		/* lang=c#-test */ "[|Assert.Equal(true, actual)|]",
-		/* lang=c#-test */ "Assert.True(actual)")]
-	[InlineData(
-		/* lang=c#-test */ "[|Assert.StrictEqual(false, actual)|]",
-		/* lang=c#-test */ "Assert.False(actual)")]
-	[InlineData(
-		/* lang=c#-test */ "[|Assert.StrictEqual(true, actual)|]",
-		/* lang=c#-test */ "Assert.True(actual)")]
-	[InlineData(
-		/* lang=c#-test */ "[|Assert.NotEqual(false, actual)|]",
-		/* lang=c#-test */ "Assert.True(actual)")]
-	[InlineData(
-		/* lang=c#-test */ "[|Assert.NotEqual(true, actual)|]",
-		/* lang=c#-test */ "Assert.False(actual)")]
-	[InlineData(
-		/* lang=c#-test */ "[|Assert.NotStrictEqual(false, actual)|]",
-		/* lang=c#-test */ "Assert.True(actual)")]
-	[InlineData(
-		/* lang=c#-test */ "[|Assert.NotStrictEqual(true, actual)|]",
-		/* lang=c#-test */ "Assert.False(actual)")]
-	public async Task ConvertsToBooleanAssert(
-		string beforeAssert,
-		string afterAssert)
+	[Fact]
+	public async Task FixAll_ConvertsAllToBooleanAsserts()
 	{
-		var before = string.Format(template, beforeAssert);
-		var after = string.Format(template, afterAssert);
+		var before = /* lang=c#-test */ """
+			using Xunit;
 
-		await Verify.VerifyCodeFix(before, after, AssertEqualShouldNotBeUsedForBoolLiteralCheckFixer.Key_UseAlternateAssert);
+			public class TestClass {
+				[Fact]
+				public void TestMethod() {
+					var actual = true;
+
+					[|Assert.Equal(false, actual)|];
+					[|Assert.Equal(true, actual)|];
+				}
+			}
+			""";
+		var after = /* lang=c#-test */ """
+			using Xunit;
+
+			public class TestClass {
+				[Fact]
+				public void TestMethod() {
+					var actual = true;
+
+					Assert.False(actual);
+					Assert.True(actual);
+				}
+			}
+			""";
+
+		await Verify.VerifyCodeFixFixAll(before, after, AssertEqualShouldNotBeUsedForBoolLiteralCheckFixer.Key_UseAlternateAssert);
 	}
 }
