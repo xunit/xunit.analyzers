5ae1530d608bb5b232ea3dbe9c95b27f89bd0d27
James Hughes <james@pyrosoftsolutions.co.uk>
2026-02-12 01:20:04 +0000

    Enabled Fix All provider for AssertEqualsShouldNotBeUsed and replaced single-fix test with multi-fix test

--- src/xunit.analyzers.tests/Fixes/X2000/AssertEqualsShouldNotBeUsedFixerTests.cs
+++ src/xunit.analyzers.tests/Fixes/X2000/AssertEqualsShouldNotBeUsedFixerTests.cs
@@ -5,33 +5,36 @@ using Verify = CSharpVerifier<Xunit.Analyzers.AssertEqualsShouldNotBeUsed>;
 
 public class AssertEqualsShouldNotBeUsedFixerTests
 {
-	const string template = /* lang=c#-test */ """
-		using Xunit;
-
-		public class TestClass {{
-			[Fact]
-			public void TestMethod() {{
-				var data = 1;
-
-				{0};
-			}}
-		}}
-		""";
-
-	[Theory]
-	[InlineData(
-		/* lang=c#-test */ "{|CS0619:[|Assert.Equals(1, data)|]|}",
-		/* lang=c#-test */ "Assert.Equal(1, data)")]
-	[InlineData(
-		/* lang=c#-test */ "{|CS0619:[|Assert.ReferenceEquals(1, data)|]|}",
-		/* lang=c#-test */ "Assert.Same(1, data)")]
-	public async Task ConvertsObjectCallToAssert(
-		string beforeAssert,
-		string afterAssert)
+	[Fact]
+	public async Task FixAll_ReplacesAllEqualsAndReferenceEqualsCalls()
 	{
-		var before = string.Format(template, beforeAssert);
-		var after = string.Format(template, afterAssert);
+		var before = /* lang=c#-test */ """
+			using Xunit;
 
-		await Verify.VerifyCodeFix(before, after, AssertEqualsShouldNotBeUsedFixer.Key_UseAlternateAssert);
+			public class TestClass {
+				[Fact]
+				public void TestMethod() {
+					var data = 1;
+
+					{|CS0619:[|Assert.Equals(1, data)|]|};
+					{|CS0619:[|Assert.ReferenceEquals(1, data)|]|};
+				}
+			}
+			""";
+		var after = /* lang=c#-test */ """
+			using Xunit;
+
+			public class TestClass {
+				[Fact]
+				public void TestMethod() {
+					var data = 1;
+
+					Assert.Equal(1, data);
+					Assert.Same(1, data);
+				}
+			}
+			""";
+
+		await Verify.VerifyCodeFixFixAll(before, after, AssertEqualsShouldNotBeUsedFixer.Key_UseAlternateAssert);
 	}
 }
