18971680ca9ff69ba4d145b6e7436ae021dc53cb
James Hughes <james@pyrosoftsolutions.co.uk>
2026-02-12 01:22:58 +0000

    Enabled Fix All provider for AssertStringEqualityCheckShouldNotUseBoolCheck and replaced single-fix test with multi-fix test

--- src/xunit.analyzers.tests/Fixes/X2000/AssertStringEqualityCheckShouldNotUseBoolCheckFixerTests.cs
+++ src/xunit.analyzers.tests/Fixes/X2000/AssertStringEqualityCheckShouldNotUseBoolCheckFixerTests.cs
@@ -5,56 +5,38 @@ using Verify = CSharpVerifier<Xunit.Analyzers.AssertStringEqualityCheckShouldNot
 
 public class AssertStringEqualityCheckShouldNotUseBoolCheckFixerTests
 {
-	const string template = /* lang=c#-test */ """
-		using System;
-		using Xunit;
-
-		public class TestClass {{
-			[Fact]
-			public void TestMethod() {{
-				var data = "foo bar baz";
-
-				{0};
-			}}
-		}}
-		""";
-
-	[Theory]
-	// Instance Equals (true)
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.True(""foo bar baz"".Equals(data))|]",
-		/* lang=c#-test */ @"Assert.Equal(""foo bar baz"", data)")]
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.True(""foo bar baz"".Equals(data, StringComparison.Ordinal))|]",
-		/* lang=c#-test */ @"Assert.Equal(""foo bar baz"", data)")]
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.True(""foo bar baz"".Equals(data, StringComparison.OrdinalIgnoreCase))|]",
-		/* lang=c#-test */ @"Assert.Equal(""foo bar baz"", data, ignoreCase: true)")]
-	// Static Equals (true)
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.True(string.Equals(""foo bar baz"", data))|]",
-		/* lang=c#-test */ @"Assert.Equal(""foo bar baz"", data)")]
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.True(string.Equals(""foo bar baz"", data, StringComparison.Ordinal))|]",
-		/* lang=c#-test */ @"Assert.Equal(""foo bar baz"", data)")]
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.True(string.Equals(""foo bar baz"", data, StringComparison.OrdinalIgnoreCase))|]",
-		/* lang=c#-test */ @"Assert.Equal(""foo bar baz"", data, ignoreCase: true)")]
-	// Instance Equals (false)
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.False(""foo bar baz"".Equals(data))|]",
-		/* lang=c#-test */ @"Assert.NotEqual(""foo bar baz"", data)")]
-	// Static Equals (false)
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.False(string.Equals(""foo bar baz"", data))|]",
-		/* lang=c#-test */ @"Assert.NotEqual(""foo bar baz"", data)")]
-	public async Task ConvertsBooleanAssertToEqualityAssert(
-		string beforeAssert,
-		string afterAssert)
+	[Fact]
+	public async Task FixAll_ReplacesAllBooleanStringEqualityChecks()
 	{
-		var before = string.Format(template, beforeAssert);
-		var after = string.Format(template, afterAssert);
+		var before = /* lang=c#-test */ """
+			using System;
+			using Xunit;
 
-		await Verify.VerifyCodeFix(before, after, AssertStringEqualityCheckShouldNotUseBoolCheckFixer.Key_UseAlternateAssert);
+			public class TestClass {
+				[Fact]
+				public void TestMethod() {
+					var data = "foo bar baz";
+
+					[|Assert.True("foo bar baz".Equals(data))|];
+					[|Assert.False("foo bar baz".Equals(data))|];
+				}
+			}
+			""";
+		var after = /* lang=c#-test */ """
+			using System;
+			using Xunit;
+
+			public class TestClass {
+				[Fact]
+				public void TestMethod() {
+					var data = "foo bar baz";
+
+					Assert.Equal("foo bar baz", data);
+					Assert.NotEqual("foo bar baz", data);
+				}
+			}
+			""";
+
+		await Verify.VerifyCodeFixFixAll(before, after, AssertStringEqualityCheckShouldNotUseBoolCheckFixer.Key_UseAlternateAssert);
 	}
 }
