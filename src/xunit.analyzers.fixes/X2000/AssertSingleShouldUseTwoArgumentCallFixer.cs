33b24240b574c4b672e20a8003544b48d6b994c4
James Hughes <james@pyrosoftsolutions.co.uk>
2026-02-12 01:22:29 +0000

    Enabled Fix All provider for AssertSingleShouldUseTwoArgumentCall and replaced single-fix test with multi-fix test

--- src/xunit.analyzers.fixes/X2000/AssertSingleShouldUseTwoArgumentCallFixer.cs
+++ src/xunit.analyzers.fixes/X2000/AssertSingleShouldUseTwoArgumentCallFixer.cs
@@ -6,6 +6,7 @@ using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editing;
+using static Microsoft.CodeAnalysis.CodeFixes.WellKnownFixAllProviders;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Xunit.Analyzers.Fixes;
@@ -18,6 +19,8 @@ public class AssertSingleShouldUseTwoArgumentCallFixer : XunitCodeFixProvider
 	public AssertSingleShouldUseTwoArgumentCallFixer() :
 		base(Descriptors.X2031_AssertSingleShouldUseTwoArgumentCall.Id)
 	{ }
+
+	public override FixAllProvider? GetFixAllProvider() => BatchFixer;
 
 	public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
 	{
--- src/xunit.analyzers.tests/Fixes/X2000/AssertSingleShouldUseTwoArgumentCallFixerTests.cs
+++ src/xunit.analyzers.tests/Fixes/X2000/AssertSingleShouldUseTwoArgumentCallFixerTests.cs
@@ -5,39 +5,38 @@ using Verify = CSharpVerifier<Xunit.Analyzers.AssertSingleShouldUseTwoArgumentCa
 
 public class AssertSingleShouldUseTwoArgumentCallFixerTests
 {
-	const string template = /* lang=c#-test */ """
-		using System.Linq;
-		using Xunit;
-
-		public class TestClass {{
-			[Fact]
-			public void TestMethod() {{
-				var list = new[] {{ -1, 0, 1, 2 }};
-
-				{0};
-			}}
-
-			public bool IsEven(int num) => num % 2 == 0;
-		}}
-		""";
-
-	[Theory]
-	[InlineData(
-		/* lang=c#-test */ "[|Assert.Single(list.Where(f => f > 0))|]",
-		/* lang=c#-test */ "Assert.Single(list, f => f > 0)")]
-	[InlineData(
-		/* lang=c#-test */ "[|Assert.Single(list.Where(n => n == 1))|]",
-		/* lang=c#-test */ "Assert.Single(list, n => n == 1)")]
-	[InlineData(
-		/* lang=c#-test */ "[|Assert.Single(list.Where(IsEven))|]",
-		/* lang=c#-test */ "Assert.Single(list, IsEven)")]
-	public async Task FixerReplacesAssertSingleOneArgumentToTwoArgumentCall(
-		string beforeAssert,
-		string afterAssert)
+	[Fact]
+	public async Task FixAll_ReplacesAllSingleOneArgumentCalls()
 	{
-		var before = string.Format(template, beforeAssert);
-		var after = string.Format(template, afterAssert);
+		var before = /* lang=c#-test */ """
+			using System.Linq;
+			using Xunit;
 
-		await Verify.VerifyCodeFix(before, after, AssertSingleShouldUseTwoArgumentCallFixer.Key_UseTwoArguments);
+			public class TestClass {
+				[Fact]
+				public void TestMethod() {
+					var list = new[] { -1, 0, 1, 2 };
+
+					[|Assert.Single(list.Where(f => f > 0))|];
+					[|Assert.Single(list.Where(n => n == 1))|];
+				}
+			}
+			""";
+		var after = /* lang=c#-test */ """
+			using System.Linq;
+			using Xunit;
+
+			public class TestClass {
+				[Fact]
+				public void TestMethod() {
+					var list = new[] { -1, 0, 1, 2 };
+
+					Assert.Single(list, f => f > 0);
+					Assert.Single(list, n => n == 1);
+				}
+			}
+			""";
+
+		await Verify.VerifyCodeFixFixAll(before, after, AssertSingleShouldUseTwoArgumentCallFixer.Key_UseTwoArguments);
 	}
 }
