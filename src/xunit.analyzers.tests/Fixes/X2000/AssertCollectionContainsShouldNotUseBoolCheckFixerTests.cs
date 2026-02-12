af36174047e86f43ca65a20044d290e9e90b1821
James Hughes <james@pyrosoftsolutions.co.uk>
2026-02-12 01:09:20 +0000

    Enabled Fix All provider for AssertCollectionContainsShouldNotUseBoolCheck and replaced single-fix test with multi-fix test

--- src/xunit.analyzers.tests/Fixes/X2000/AssertCollectionContainsShouldNotUseBoolCheckFixerTests.cs
+++ src/xunit.analyzers.tests/Fixes/X2000/AssertCollectionContainsShouldNotUseBoolCheckFixerTests.cs
@@ -5,47 +5,40 @@ using Verify = CSharpVerifier<Xunit.Analyzers.AssertCollectionContainsShouldNotU
 
 public class AssertCollectionContainsShouldNotUseBoolCheckFixerTests
 {
-	const string template = /* lang=c#-test */ """
-		using System;
-		using System.Linq;
-		using Xunit;
-
-		public class TestClass {{
-			[Fact]
-			public void TestMethod() {{
-				var items = new[] {{ "a", "b", "c" }};
-
-				{0};
-			}}
-		}}
-		""";
-
-	[Theory]
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.True(items.Contains(""b""))|]",
-		/* lang=c#-test */ @"Assert.Contains(""b"", items)")]
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.True(items.Contains(""b"", StringComparer.Ordinal))|]",
-		/* lang=c#-test */ @"Assert.Contains(""b"", items, StringComparer.Ordinal)")]
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.True(items.Contains(""b"", null))|]",
-		/* lang=c#-test */ @"Assert.Contains(""b"", items)")]
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.False(items.Contains(""b""))|]",
-		/* lang=c#-test */ @"Assert.DoesNotContain(""b"", items)")]
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.False(items.Contains(""b"", StringComparer.Ordinal))|]",
-		/* lang=c#-test */ @"Assert.DoesNotContain(""b"", items, StringComparer.Ordinal)")]
-	[InlineData(
-		/* lang=c#-test */ @"[|Assert.False(items.Contains(""b"", null))|]",
-		/* lang=c#-test */ @"Assert.DoesNotContain(""b"", items)")]
-	public async Task ReplacesBooleanAssert(
-		string beforeAssert,
-		string afterAssert)
+	[Fact]
+	public async Task FixAll_ReplacesAllBooleanAsserts()
 	{
-		var before = string.Format(template, beforeAssert);
-		var after = string.Format(template, afterAssert);
+		var before = /* lang=c#-test */ """
+			using System;
+			using System.Linq;
+			using Xunit;
 
-		await Verify.VerifyCodeFix(before, after, AssertCollectionContainsShouldNotUseBoolCheckFixer.Key_UseAlternateAssert);
+			public class TestClass {
+				[Fact]
+				public void TestMethod() {
+					var items = new[] { "a", "b", "c" };
+
+					[|Assert.True(items.Contains("b"))|];
+					[|Assert.False(items.Contains("b"))|];
+				}
+			}
+			""";
+		var after = /* lang=c#-test */ """
+			using System;
+			using System.Linq;
+			using Xunit;
+
+			public class TestClass {
+				[Fact]
+				public void TestMethod() {
+					var items = new[] { "a", "b", "c" };
+
+					Assert.Contains("b", items);
+					Assert.DoesNotContain("b", items);
+				}
+			}
+			""";
+
+		await Verify.VerifyCodeFixFixAll(before, after, AssertCollectionContainsShouldNotUseBoolCheckFixer.Key_UseAlternateAssert);
 	}
 }
