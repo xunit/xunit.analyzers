574f1436829f7080c285f422fb2199f8e2c01df4
James Hughes <james@pyrosoftsolutions.co.uk>
2026-02-12 01:10:11 +0000

    Enabled Fix All provider for AssertEmptyCollectionCheckShouldNotBeUsed and replaced single-fix test with multi-fix test

--- src/xunit.analyzers.fixes/X2000/AssertEmptyCollectionCheckShouldNotBeUsedFixer.cs
+++ src/xunit.analyzers.fixes/X2000/AssertEmptyCollectionCheckShouldNotBeUsedFixer.cs
@@ -6,6 +6,7 @@ using Microsoft.CodeAnalysis.CodeActions;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editing;
+using static Microsoft.CodeAnalysis.CodeFixes.WellKnownFixAllProviders;
 using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
 
 namespace Xunit.Analyzers.Fixes;
@@ -19,6 +20,8 @@ public class AssertEmptyCollectionCheckShouldNotBeUsedFixer : XunitCodeFixProvid
 	public AssertEmptyCollectionCheckShouldNotBeUsedFixer() :
 		base(Descriptors.X2011_AssertEmptyCollectionCheckShouldNotBeUsed.Id)
 	{ }
+
+	public override FixAllProvider? GetFixAllProvider() => BatchFixer;
 
 	public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
 	{
