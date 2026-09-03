5aafb1d1051dd8571092e8020b001e327242a3c3
Brad Wilson <dotnetguy@gmail.com>
2026-09-03 10:37:15 -0700

    xunit/xunit#3622: Update xUnit1037 tests for `ClassDataAttribute<T>`

--- src/xunit.analyzers.tests/Analyzers/X1000/X1037_ClassDataAttributeMustPointAtValidClassTests.cs
+++ src/xunit.analyzers.tests/Analyzers/X1000/X1037_ClassDataAttributeMustPointAtValidClassTests.cs
@@ -21,10 +21,17 @@ public class X1037_ClassDataAttributeMustPointAtValidClassTests
 				[Theory]
 				[{|#0:ClassData(typeof(DataClass))|}]
 				public void TestMethod(int n, string f) { }
+
+				[Theory]
+				[{|#1:ClassData<DataClass>|}]
+				public void TestMethod_Generic(int n, string f) { }
 			}
 			""";
-		var expected = Verify.Diagnostic("xUnit1037").WithLocation(0).WithArguments("Xunit.TheoryDataRow");
+		var expected = new[] {
+			Verify.Diagnostic("xUnit1037").WithLocation(0).WithArguments("Xunit.TheoryDataRow"),
+			Verify.Diagnostic("xUnit1037").WithLocation(1).WithArguments("Xunit.TheoryDataRow"),
+		};
 
-		await Verify.VerifyAnalyzerV3(LanguageVersion.CSharp7_1, source, expected);
+		await Verify.VerifyAnalyzerV3(LanguageVersion.CSharp11, source, expected);
 	}
 }
